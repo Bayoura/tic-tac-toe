e18a31b6f8f37fb6d55e87d29d72b1fb067a4068
Bayoura <Kristin.Seidelmann@gmx.de>
2022-01-26 21:24:14 +0100

    add basic event listener

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 const panels_button = document.querySelectorAll('[data-panel]');
 const restart_button = document.querySelector('[data-restart]');
+
+let round = 0;
+panels_button.forEach(panel => {
+    panel.addEventListener('click', () => {
+        
+        if (panel.innerText != '') return;
+        if(round % 2 == 0) {
+            panel.innerText = marks[0];
+            round++;
+        } else {
+            panel.innerText = marks[1];
+            round++;
+        }
+    })        
+})
+
+let marks = ['X','O'];
