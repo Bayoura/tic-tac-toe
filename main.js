48d01af3af4fccfe00d8e46064252325c019e29f
Bayoura <Kristin.Seidelmann@gmx.de>
2022-01-28 15:31:19 +0100

    add basic checkForWin function and announceResult function

--- main.js
+++ main.js
@@ -7,35 +7,59 @@ const overlay_div = document.querySelector('[data-overlay]');
 let gameboardModule = (function() {
     // let gameboard = [];
     let round = 0;
-
+    
     let render = function(panel) {
         if (panel.innerText != '') {
             return;
         } else if (round % 2 == 0) {
             panel.innerText = signs[0];    
+            checkForWin();
             round++;
         } else {
             panel.innerText = signs[1];
+            checkForWin();
             round++;
         }
     }
 
     let checkForWin = function() {
-        //announceResult()
-        
+        if (panels_button[0].innerText != '' && panels_button[0].innerText == panels_button[1].innerText && panels_button[0].innerText == panels_button[2].innerText) {
+            announceResult(panels_button[0].innerText);     
+        } else if (panels_button[3].innerText != '' && panels_button[3].innerText == panels_button[4].innerText && panels_button[4].innerText == panels_button[5].innerText) {
+            announceResult(panels_button[3].innerText);     
+        } else if (panels_button[6].innerText != '' && panels_button[6].innerText == panels_button[7].innerText && panels_button[7].innerText == panels_button[8].innerText) {
+            announceResult(panels_button[6].innerText); 
+        } else if (panels_button[0].innerText != '' && panels_button[0].innerText == panels_button[3].innerText && panels_button[3].innerText == panels_button[6].innerText) {
+            announceResult(panels_button[0].innerText);     
+        } else if (panels_button[1].innerText != '' && panels_button[1].innerText == panels_button[4].innerText && panels_button[4].innerText == panels_button[7].innerText) {
+            announceResult(panels_button[1].innerText); 
+        } else if (panels_button[2].innerText != '' && panels_button[2].innerText == panels_button[5].innerText && panels_button[5].innerText == panels_button[8].innerText) {
+            announceResult(panels_button[2].innerText); 
+        } else if (panels_button[0].innerText != '' && panels_button[0].innerText == panels_button[4].innerText && panels_button[4].innerText == panels_button[8].innerText) {
+            announceResult(panels_button[0].innerText); 
+        } else if (panels_button[2].innerText != '' && panels_button[2].innerText == panels_button[4].innerText && panels_button[4].innerText == panels_button[6].innerText) {
+            announceResult(panels_button[2].innerText); 
+        } else if (round == 8) {
+            announceResult('T');
+        }       
     }
 
-    let announceResult = function() {
+    let announceResult = function(winner) {
         overlay_div.classList.add('active');
         modal_div.classList.add('active');
-        //check if it's a tie or not
-        //display_div.innerText = `Player ${} has won this game!` / 'It\'s a tie!';
+        if (winner == 'T') {
+            result_p.innerText = 'It\'s a tie!';
+        } else {
+            result_p.innerText = `${winner} has won this game!`;
+        }
     }
 
     let clearGameboard = function() {
+        overlay_div.classList.remove('active');
+        modal_div.classList.remove('active');
         result_p.innerText = '';
+        round = 0;
         panels_button.forEach(panel => panel.innerText = ''); 
-        //clear array
     }
 
     return {
@@ -56,7 +80,7 @@ function playerFactory() {
 panels_button.forEach(panel => {
     panel.addEventListener('click', () => {
         gameboardModule.render(panel);
-        gameboardModule.checkForWin();
+        // gameboardModule.checkForWin();
     })
 })
 
@@ -65,3 +89,5 @@ restart_button.addEventListener('click', () => {
 });
 
 let signs = ['X','O'];
+
+
