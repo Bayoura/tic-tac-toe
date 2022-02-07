ab6e1745c10e1d16344258067daea8ff3f8a18e0
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-07 12:28:58 +0100

    add createPlayers function, change startGame function and correctly implement the newGame button

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const namesInput_div = document.querySelector('[data-namesInput]');
 const inputForm_form = document.querySelector('[data-inputForm]')
 const xName_input = document.getElementById('name-player-one');
 const oName_input = document.getElementById('name-player-two');
+const xName_div = document.getElementById('player-one')
+const oName_div = document.getElementById('player-two')
 const submitNames_button = document.querySelector('[data-enterNamesButton]');
 const gameboard_div = document.querySelector('[data-gameboard]');
 const cells_button = document.querySelectorAll('[data-cell]');
@@ -17,8 +19,8 @@ let gameboardModule = (function() {
     let turnX;
     const x = 'x';
     const o = 'o';
-    let xScore = 0;
-    let oScore = 0;
+    // let xScore = 0;
+    // let oScore = 0;
     const winCombos = [
         [0,1,2],
         [3,4,5],
@@ -28,31 +30,53 @@ let gameboardModule = (function() {
         [2,5,8],
         [0,4,8],
         [2,4,6]
-    ]
+    ];
+
+    let playerX;
+    let playerO;
 
     let startGame = function() {
         turnX = true;
         updateHoverMark();
-        namesInput_div.classList.remove('active'); 
         resultScreen_div.classList.remove('active');
         result_p.innerText = '';
 
         cells_button.forEach(cell => {
             cell.classList.remove(x);
             cell.classList.remove(o);
-            cell.removeEventListener('click', render); //IMPORTANT!! Otherwise the event listeners will stack
+            cell.removeEventListener('click', render); // IMPORTANT!! Otherwise the event listeners will stack
             cell.addEventListener('click', render, { once: true }); // `once: true` ensures that the event listener only fires once for each element
         })
 
         newRound_button.addEventListener('click', startGame);
         newGame_button.addEventListener('click', () => {
+            resultScreen_div.classList.remove('active');
             namesInput_div.classList.add('active');
-            xScore = 0;
-            oScore = 0;
-            xScore_span.innerText = xScore;
-            oScore_span.innerText = oScore;
             startGame();
         });
+
+        document.addEventListener('DOMContentLoaded', () => {    
+            submitNames_button.addEventListener('click', e => {    
+                e.preventDefault(); //stop form from submitting   
+        
+                //check if required fields are filled out
+                let checkStatus = inputForm_form.checkValidity();
+                inputForm_form.reportValidity();
+                if (checkStatus) {
+                    createPlayers();
+                    namesInput_div.classList.remove('active'); 
+                    xScore_span.innerText = playerX.score;
+                    oScore_span.innerText = playerO.score;
+                    xName_div.innerText = playerX.name;
+                    oName_div.innerText = playerO.name;
+                }
+            })    
+        });
+    }
+
+    let createPlayers = function() {
+        playerX = playerFactory(xName_input.value);
+        playerO = playerFactory(oName_input.value);
     }
 
     let render = function(clickedCell) {
@@ -73,7 +97,7 @@ let gameboardModule = (function() {
         turnX = !turnX;
     }
 
-    let updateHoverMark = function(nextTurn) {
+    let updateHoverMark = function() {
         gameboard_div.classList.remove(x);
         gameboard_div.classList.remove(o);
         if (turnX) {
@@ -83,18 +107,20 @@ let gameboardModule = (function() {
         }
     }
 
-    let endGame = function(isDraw, currentTurn) {
+    let endGame = function(isDraw) {
         if (isDraw) {
             result_p.innerText = 'It\'s a draw!';
         } else {
             if (turnX) {
-                xScore++;
-                xScore_span.innerText = xScore;
+                playerX.score++;
+                xScore_span.innerText = playerX.score;
+                result_p.innerText = `${playerX.name} is the winner!`;
             } else {
-                oScore++;
-                oScore_span.innerText = oScore;
+                playerO.score++;
+                oScore_span.innerText = playerO.score;
+                result_p.innerText = `${playerO.name} is the winner!`;
             }
-            result_p.innerText = `${currentTurn.toUpperCase()} is the winner!`;
+            
         }
         resultScreen_div.classList.add('active');
     }
@@ -118,25 +144,12 @@ let gameboardModule = (function() {
     }
 })();
 
-document.addEventListener('DOMContentLoaded', () => {    
-    submitNames_button.addEventListener('click', e => {    
-        e.preventDefault(); //stop form from submitting   
+function playerFactory(name) {
+    const player = {};
+    player.name = name;
+    player.score = 0;
+    return player;
+}
 
-        //check if required fields are filled out
-        let checkStatus = inputForm_form.checkValidity();
-        inputForm_form.reportValidity();
-        if (checkStatus) {
-            gameboardModule.startGame();
-        }
-    })    
-});
+gameboardModule.startGame();
 
-// function playerFactory(name) {
-//     const player = {};
-//     player.name = name;
-//     return player;
-// }
-
-// const playerOne = playerFactory(xName_input.value);
-
-//pass playerOne and playerTwo into startGame() ?
